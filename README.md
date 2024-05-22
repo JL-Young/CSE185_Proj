c6b26ea77bd2cfab19441985dfbecbd428f337f3
jjjjjl24 <lijane2004@gmail.com>
2024-05-22 13:29:05 -0700

    mytsne script added with plotting

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# CSE185_Proj
+# CSE185_Proj: Mytsne
+
+This tool implements tsne analysis and plotting. The main tsne calculation tool takes a path for a adata object and outputs a n_obs x 2 matrix that contains the x and y coordinates for plotting for each sample. Then the tsne plotting function takes the n_obs x 2 matrix and generate the tsne plot. 
+
+## Installation
+
+## Data preprocessing
+
+## Testing 
+
+## Benchmarking
---
+++ mytsne/mytsne.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import scanpy as sc
+
+def mytsne(adata, perp = 30):
+
+
+
+# can have the coordinates already generated, or just have a adata object (can run tsne in this function)
+def mytsne_plot(adata, coor = None, perp = 30, xlabel = "tsne1", ylabel = "tsne 2", title = "", save = None):
+    # import the n_obs x 2 (coordinates)
+    if "leiden" not in adata.obs: 
+        raise Exception("anndata object is not clustered with Leiden or the Leiden cluster values are not stored in a column named leiden in adata.obs.")
+    # if coor = NONE: 
+    #     coor = mytsne(adata, perp)
+
+    plt.scatter(coors[:,0], coors[:,1], c = adata.obs["leiden"])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show
+
+    if save is not None: 
+        plt.savefig(save)
+
