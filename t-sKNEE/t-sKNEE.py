221ec98a1f8cb83dabb7ea86239426823ba3ac28
jjjjjl24 <lijane2004@gmail.com>
2024-05-24 08:38:53 -0700

    README and t-sknee.py changed

--- t-sKNEE/t-sKNEE.py
+++ t-sKNEE/t-sKNEE.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import scanpy as sc
 
 def tsKNEE(adata, perp = 30):
+    if "leiden" not in adata.obs: 
+        raise Exception("Anndata object is not clustered with Leiden or the Leiden cluster values are not stored in a column named leiden in adata.obs.")
 
 
 
 # can have the coordinates already generated, or just have a adata object (can run tsne in this function)
-def mytsne_plot(adata, coor = None, perp = 30, xlabel = "tsne1", ylabel = "tsne 2", title = "", save = None):
+def tsKNEE_plot(adata, coor = None, perp = 30, xlabel = "tsne1", ylabel = "tsne 2", title = "", save = None):
     # import the n_obs x 2 (coordinates)
     if "leiden" not in adata.obs: 
-        raise Exception("anndata object is not clustered with Leiden or the Leiden cluster values are not stored in a column named leiden in adata.obs.")
+        raise Exception("Anndata object is not clustered with Leiden or the Leiden cluster values are not stored in a column named leiden in adata.obs.")
     # if coor = NONE: 
     #     coor = tsKNEE(adata, perp)
 
