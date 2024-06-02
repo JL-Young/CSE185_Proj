31ce2e7caafaaabfd69c14be263317f461e35a0c
Jeyasri Venkat <137555229+jey-11@users.noreply.github.com>
2024-06-02 11:15:55 -0700

    changes to the setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 setup(
     name='t_sKNEE',
+    version='1.0.0',  # You can specify the version
     description='CSE1185 Final Project',
-    authors='Li, Venkatasubramani, Young',
-    author_emails='jal098@ucsd.edu, jvenkatasubramani@ucsd.edu, jlyoung@ucsd.edu',
+    author='Li, Venkatasubramani, Young',
+    author_email='jal098@ucsd.edu, jvenkatasubramani@ucsd.edu, jlyoung@ucsd.edu',
     url='https://github.com/JL-Young/CSE185_Proj',
+    packages=find_packages(),  # Automatically find packages in your project directory
     install_requires=[
-        "numpy",
-        "matplotlib",
-        "scanpy",
-        "leidenalg",
-        "random"
+        'numpy',
+        'matplotlib',
+        'scanpy',
+        'leidenalg'
     ],
 )
